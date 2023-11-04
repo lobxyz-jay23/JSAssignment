00eb841b76e60700753ce38d195dfc19228e0a74
lobxyz-jay23 <145226936+lobxyz-jay23@users.noreply.github.com>
2023-11-04 17:32:50 +0100

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,9 @@
+
+
+let num = 5;
+let fact = 1;
+
+for ( i=1; 1 <= 5; i++) {
+    fact = fact * 1;
+}
+console.log(fact);
---
+++ script.js
@@ -0,0 +1,64 @@
+// LEAP YEAR
+
+// let year = prompt ("Enter a year")
+
+// if (year % 4 === 0){
+//     if (year % 100 === 0){ 
+//         if (year % 400 === 0){
+//             console.log(year + "is a leap year");
+
+//         }else {
+//             console.log( year + "year is not a leap year");
+//         }
+        
+//     }else{ 
+//         console.log(year + "is a leap year");
+//     }
+    
+
+// }else {
+//     console.log(year + " is not a leap year");
+
+// }
+
+// FACTORIAL CALCULATOR 
+
+// let num = 5;
+// let fact = 1;
+
+// for ( i=1; 1 <= 5; i++) {
+//     fact = fact * 1;
+// }
+// console.log(fact);
+
+
+// ARRAY FILTERING
+
+// const numbers = [-1, 1, 2 , -2, 3, 4]
+
+// const filtered = numbers.filter ( value => value >= 0)
+
+// console.log(filtered);
+
+// RANDOM NUMBER GENERATOR BTW 1 - 5
+
+// var random = Math.floor (Math.random ( ) * 5) + 1;
+
+// console.log(random);
+
+// ARRAY REVERSE
+
+// const numbers = [1,2,3,4,5,6];
+
+// numbers.reverse();
+
+// console.log (numbers);
+
+// STRING CAPITALIZATION
+
+    // let str = 'babajide';
+    // let string =str.toUpperCase;
+    
+    // console.log(string);
+
+
